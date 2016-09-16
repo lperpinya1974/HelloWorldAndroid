61d0e4607257fbd30a5d31d79fb8814088346533
lperpinya1974 <lperpinya1974@gmail.com>
2016-09-16 11:12:14 +0200

    Initial commit

---
+++ app/src/main/java/com/exemple/profedam/helloworldandroid/MainHelloWorld.java
@@ -0,0 +1,14 @@
+package com.exemple.profedam.helloworldandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hello_world);
+    }
+}
